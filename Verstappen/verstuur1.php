c5629f0c3201fb0d8e9077019862c959fcc55ba4
Ryan-Habets <92308864+Ryan-Habets@users.noreply.github.com>
2022-02-18 16:49:25 +0100

    Add files via upload

---
+++ Verstappen/verstuur1.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Verstappen</title>
+    <link rel="stylesheet" href="styleee.css">
+</head>
+<body>
+
+<img class="background" src="./images/duim.jpg">
+
+<div class="shapes">
+    <div class="box1">
+        <div class="text1">
+            <h1>Bedankt voor uw aanmelding!
+                <br>
+                <br>
+                U zult zo snel mogelijk een goedkeuringsmail krijgen voor de reservering van de Meet&Greet!</h1>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ Verstappen/verstuur2.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Verstappen</title>
+    <link rel="stylesheet" href="styleee.css">
+</head>
+<body>
+
+<div class="shapes">
+    <div class="box2">
+        <div class="text2">
+            <h1>Er is geconstateerd dat u  onder 18  bent. Natuurlijk willen wij als Red Bull dat uw veiligheid voorop gaat! Daarom willen wij graag toestemming krijgen van uw begeleider/ouder. </h1>
+        </div>
+    </div>
+    <div class="box3">
+        <div class="text3">
+            <h1>Voor de Begeleider/ouder;
+            <br>
+            <br>
+            <br>
+            <br>
+                Voer hieronder uw e-mail adres in, er wordt daarna een mail verstuurd met de volgende stappen.
+            </h1>
+
+        </div>
+        <div class="InputForm2">
+            <form method="post" target="_self">
+                <input type="email" name="email" placeholder="Email" required/>
+                <input class="Send" type="submit" value="SEND"/>
+            </form>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
