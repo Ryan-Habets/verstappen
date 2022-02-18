5b184bf1ebce5e53cbf5ec6866a4655a9aeaad91
Ryan-Habets <92308864+Ryan-Habets@users.noreply.github.com>
2022-02-18 16:41:36 +0100

    Add files via upload

---
+++ Verstappen/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Verstappen</title>
+    <link rel="stylesheet" href="stylesheet.css">
+</head>
+<body>
+
+
+
+    <div class="Header">
+        <img class="Gif" src="./images/ComplicatedUnderstatedCub-size_restricted.gif">
+        <img class="Max" src="./images/Max.png">
+        <img class="RedBull" src="./images/Red-Bull.png">
+    </div>
+<div class="mid">
+    <div class="textbox">
+        <p>In 2022 zal Max voor het eerst met startnummer 1 op zijn auto rijden en probeert hij zijn titel met succes te verdedigen. Maar voordat we hem weer zien strijden op de circuits, is er nog een mogelijkheid om een Meet&Greet te hebben met onze kampioen!
+            Op Dinsdag 1 maart, woensdag 2 Maart en vrijdag 4 Maart is er de mogelijk om Max te kunnen ontmoeten! Deze Meet&Greet wordt gehouden op 3 verschillende plekken in Nederland. Er zijn maar een bepaald aantal tijden dus wees er snel bij!
+        </p>
+        <br>
+        <br>
+        <br>
+        <br>
+
+       <div class="textbox2"> <p>
+                Als u hieronder klikt, dan kunt u naar het formulier gaan om u op te geven voor de Meet&Greet!
+            </p>
+       </div>
+    </div>
+
+    <a href="Form.php" class="myButton">inschrijven</a>
+
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+</div>
+
+<div class="footer">
+    <a class="login" href="login.php">Login</a>
+    <text class="text">--Made For Project--</text>
+    <a href="https://www.redbullracing.com/int-en"><img class="logo" src="./images/logo.png"></a>
+
+</div>
+</body>
+</html>
